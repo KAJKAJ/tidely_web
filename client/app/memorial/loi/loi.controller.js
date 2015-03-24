f4d099dbaee1802247d71d9ef6b1347c87976693
youngmin <youngmin.k@gmail.com>
2015-03-24 20:01:26 +0900

    loi completed

--- client/app/memorial/loi/loi.controller.js
+++ client/app/memorial/loi/loi.controller.js
@@ -9,23 +9,36 @@ angular.module('doresolApp')
     $scope.currentUser = User.getCurrentUser();
 
     $scope.users = User.getUsersObject();
-    $scope.members = {};
-    $scope.waitings = {};
-    $scope.watingsCnt = 0;
 
     $scope.memorial.$loaded().then(function(value) {
       $scope.leader = User.findById($scope.memorial.ref_user);
 
-      
-      if ($scope.memorial.letter_of_intent == null) {
-        $scope.memorial.letter_of_intent = {};
-      }
-      if ($scope.memorial.letter_of_intent.funeral == null) {
-        $scope.memorial.letter_of_intent.funeral = {};
-      }
-      if ($scope.memorial.letter_of_intent.medical_care == null) {
-        $scope.memorial.letter_of_intent.medical_care = {};
-      }
+      if (!$scope.memorial.letter_of_intent) $scope.memorial.letter_of_intent = {};
+      if (!$scope.memorial.letter_of_intent.funeral) $scope.memorial.letter_of_intent.funeral = {};
+      if (!$scope.memorial.letter_of_intent.funeral.notice) $scope.memorial.letter_of_intent.funeral.notice = null;
+      if (!$scope.memorial.letter_of_intent.funeral.guide) $scope.memorial.letter_of_intent.funeral.guide = null;
+      if (!$scope.memorial.letter_of_intent.funeral.religion) $scope.memorial.letter_of_intent.funeral.religion = null;
+      if (!$scope.memorial.letter_of_intent.funeral.duration) $scope.memorial.letter_of_intent.funeral.duration = null;
+      if (!$scope.memorial.letter_of_intent.funeral.donation) $scope.memorial.letter_of_intent.funeral.donation = null;
+      if (!$scope.memorial.letter_of_intent.funeral.food) $scope.memorial.letter_of_intent.funeral.food = null;
+      if (!$scope.memorial.letter_of_intent.funeral.body) $scope.memorial.letter_of_intent.funeral.body = null;
+
+      if (!$scope.memorial.letter_of_intent.medical_care) $scope.memorial.letter_of_intent.medical_care = {};
+      if (!$scope.memorial.letter_of_intent.medical_care.agent) $scope.memorial.letter_of_intent.medical_care.agent = {};
+      if (!$scope.memorial.letter_of_intent.medical_care.agent.name_1) $scope.memorial.letter_of_intent.medical_care.agent.name_1 = null;
+      if (!$scope.memorial.letter_of_intent.medical_care.agent.relation_1) $scope.memorial.letter_of_intent.medical_care.agent.relation_1 = null;
+      if (!$scope.memorial.letter_of_intent.medical_care.agent_tel_1) $scope.memorial.letter_of_intent.medical_care.agent_tel_1 = null;
+      if (!$scope.memorial.letter_of_intent.medical_care.agent.name_2) $scope.memorial.letter_of_intent.medical_care.agent.name_2 = null;
+      if (!$scope.memorial.letter_of_intent.medical_care.agent.relation_2) $scope.memorial.letter_of_intent.medical_care.agent.relation_2 = null;
+      if (!$scope.memorial.letter_of_intent.medical_care.agent_tel_2) $scope.memorial.letter_of_intent.medical_care.agent_tel_2 = null;
+
+      if (!$scope.memorial.letter_of_intent.medical_care.surviving_treatment) $scope.memorial.letter_of_intent.medical_care.surviving_treatment = {};
+      if (!$scope.memorial.letter_of_intent.medical_care.surviving_treatment.brain) $scope.memorial.letter_of_intent.medical_care.surviving_treatment.brain = null;
+      if (!$scope.memorial.letter_of_intent.medical_care.surviving_treatment.dying) $scope.memorial.letter_of_intent.medical_care.surviving_treatment.dying = null;
+      if (!$scope.memorial.letter_of_intent.medical_care.surviving_treatment.old) $scope.memorial.letter_of_intent.medical_care.surviving_treatment.old = null;
+      if (!$scope.memorial.letter_of_intent.medical_care.surviving_treatment.life_support) $scope.memorial.letter_of_intent.medical_care.surviving_treatment.life_support = null;
+      if (!$scope.memorial.letter_of_intent.medical_care.surviving_treatment.nutrition) $scope.memorial.letter_of_intent.medical_care.surviving_treatment.nutrition = null;
+
       User.setUsersObject($scope.memorial.ref_user);
     });
 
@@ -85,12 +98,17 @@ angular.module('doresolApp')
               medical_care: {
                 surviving_treatment: {
 
-                  brain: $scope.memorial.letter_of_intent.medical_care.surviving_treatment.brain,
-                  dying: $scope.memorial.letter_of_intent.medical_care.surviving_treatment.dying,
-                  old: $scope.memorial.letter_of_intent.medical_care.surviving_treatment.old,
+                  brain: $scope.memorial.letter_of_intent.medical_care.surviving_treatment.brain ?
+                        $scope.memorial.letter_of_intent.medical_care.surviving_treatment.brain : 0,
+                  dying: $scope.memorial.letter_of_intent.medical_care.surviving_treatment.dying ?
+                        $scope.memorial.letter_of_intent.medical_care.surviving_treatment.dying : 0,
+                  old: $scope.memorial.letter_of_intent.medical_care.surviving_treatment.old ? 
+                        $scope.memorial.letter_of_intent.medical_care.surviving_treatment.old : 0,
 
-                  life_support: $scope.memorial.letter_of_intent.medical_care.surviving_treatment.life_support,
-                  nutrition: $scope.memorial.letter_of_intent.medical_care.surviving_treatment.nutrition
+                  life_support: $scope.memorial.letter_of_intent.medical_care.surviving_treatment.life_support ?
+                                $scope.memorial.letter_of_intent.medical_care.surviving_treatment.life_support : 0,
+                  nutrition: $scope.memorial.letter_of_intent.medical_care.surviving_treatment.nutrition ?
+                              $scope.memorial.letter_of_intent.medical_care.surviving_treatment.nutrition : 0
                 },
 
                 agent: {
